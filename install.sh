e02af3c07a47da7651f7851965c6b09f64e6fc46
阿天 <112182068+atianshow@users.noreply.github.com>
2024-05-03 04:46:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # 函数：更新系统
 update_system() {
     echo "Updating system..."
@@ -18,7 +17,6 @@ update_system() {
         exit 1
     fi
 }
-
 # 函数：安装 Docker
 install_docker() {
     echo "Installing Docker..."
@@ -26,28 +24,24 @@ install_docker() {
     sudo sh get-docker.sh
     sudo usermod -aG docker $USER
 }
-
 # 函数：安装 Docker Compose
 install_docker_compose() {
     echo "Installing Docker Compose..."
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 }
-
 # 函数：安装 Portainer
 install_portainer() {
     echo "Installing Portainer..."
     docker volume create portainer_data
     docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 }
-
 # 函数：安装 Nginx Proxy Manager
 install_nginx_proxy_manager() {
     echo "Installing Nginx Proxy Manager..."
     docker volume create npm_data
     docker run -d -p 80:80 -p 443:443 -p 81:81 --name=npm --restart=always -v npm_data:/data jc21/nginx-proxy-manager:latest
 }
-
 # 主函数
 main() {
     update_system
@@ -56,6 +50,5 @@ main() {
     install_portainer
     install_nginx_proxy_manager
 }
-
 # 执行主函数
 main
