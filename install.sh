0834f8b79272a151fa688ed3b93f34e06c9851d0
阿天 <112182068+atianshow@users.noreply.github.com>
2024-05-04 06:33:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -112,9 +112,9 @@ install_wordpress1() {
     echo "正在安装 WordPress 网站 wordpress1 ..."
     docker volume create wordpress1_db
     docker volume create wordpress1_wp
-    docker run --restart always -e "[object Object]" -v wordpress1_db:/var/lib/mysql mysql:5.7
+    docker run --restart always -e MYSQL_ROOT_PASSWORD=Aa1989..@@ -e MYSQL_DATABASE=wordpress1 -v wordpress1_db:/var/lib/mysql mysql:5.7
     docker run --restart always -p 8001:80 -v wordpress1_wp:/var/www/html wordpress:latest
-        if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         echo "WordPress1 安装成功。访问 http://localhost:8001 查看状态。"
     else
         echo "WordPress1 安装失败，请检查错误信息。"
@@ -127,10 +127,10 @@ install_wordpress2() {
     echo "正在安装 WordPress 网站 wordpress2 ..."
     docker volume create wordpress2_db
     docker volume create wordpress2_wp
-    docker run --restart always -e "[object Object]" -v wordpress2_db:/var/lib/mysql mysql:5.7
+    docker run --restart always -e MYSQL_ROOT_PASSWORD=Aa1989..@@ -e MYSQL_DATABASE=wordpress2 -v wordpress2_db:/var/lib/mysql mysql:5.7
     docker run --restart always -p 8002:80 -v wordpress2_wp:/var/www/html wordpress:latest
-        if [ $? -eq 0 ]; then
-        echo "WordPress1 安装成功。访问 http://localhost:8002 查看状态。"
+    if [ $? -eq 0 ]; then
+        echo "WordPress2 安装成功。访问 http://localhost:8002 查看状态。"
     else
         echo "WordPress2 安装失败，请检查错误信息。"
         exit 1
